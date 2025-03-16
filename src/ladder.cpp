2fcf61d220f4d2a0a87937a3cbd1b27a0fdb4a27
gquinta1 <gquinta1@uci.edu>
2025-03-16 10:20:01 +0000

    generate_word_ladder method

---
+++ src/ladder.cpp
@@ -0,0 +1,51 @@
+
+
+void error(string word1, string word2, string msg){
+
+}
+
+bool edit_distance_within(const std::string& str1, const std::string& str2, int d){
+    return true;
+}
+
+bool is_adjacent(const string& word1, const string& word2){
+    return true;
+}
+
+vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list){
+    vector<string> ladder_queue;
+    ladder_queue.push(begin_word);
+    set<string> visited;
+    visited.insert(begin_word);
+    while (!ladder_queue.empty()){
+        ladder = ladder_queue.pop_front();
+        last_word = ladder.back();
+        for (string word : word_list){
+            if is_adjacent(end_word, word){
+                if (visited.find(word) != visited.end()){
+                    visited.insert(word);
+                    new_ladder = ladder;
+                    new_ladder.push_back(word);
+                    if (word==end_word){
+                        return new_ladder;
+                    }
+                    ladder_queue.push(new_ladder);
+                }
+            }
+        }
+    }
+    vector<string> null_ladder;
+    return null_ladder;
+}
+
+void load_words(set<string> & word_list, const string& file_name){
+
+}
+
+void print_word_ladder(const vector<string>& ladder){
+
+}
+
+void verify_word_ladder(){
+
+}
